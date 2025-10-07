30d410a389f776020c2ec9cecd801903e1de1404
Yves <yvesj0311@gmail.com>
2025-10-07 23:49:03 +0200

    fix: Configure Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/seeg/',
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
